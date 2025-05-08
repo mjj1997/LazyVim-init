779a29b4c763f91cd8507ec38de03e5ec2a15118
Ma Jiajun <691460087@qq.com>
2025-05-08 15:50:12 +0800

    feature: 添加跨终端复制粘贴文本功能

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,24 @@ local opt = vim.opt
 
 opt.shiftwidth = 4
 opt.tabstop = 4
+
+function my_paste(reg)
+  return function(lines)
+    --[ 返回 “” 寄存器的内容，用来作为 p 操作符的粘贴物 ]
+    local content = vim.fn.getreg('"')
+    return vim.split(content, "\n")
+  end
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    --[ 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点 ]
+    ["+"] = my_paste("+"),
+    ["*"] = my_paste("*"),
+  },
+}
